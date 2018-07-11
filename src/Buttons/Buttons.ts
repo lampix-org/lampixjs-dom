b9eb3c8ddde9df50768ad77c3cbd1638aeb572c7
Dan Balaban <dan.stefan.balaban@gmail.com>
2018-07-11 17:12:55 +0300

    Reduced button radius size to 13

--- src/Buttons/Buttons.ts
+++ src/Buttons/Buttons.ts
@@ -21,7 +21,7 @@ class Buttons implements IButtons {
 
     // the following properties cannot be overriden
     opts = Object.assign(defaultOpts, opts, {
-      radius: 25 // half the size of the button
+      radius: 13 // half the size of the button
     });
 
     // validate params
